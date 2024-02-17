15143f742f9f1ac0bff4e81895a9f8d56aeb6ca8
Sameer Singh Bhandari <bhandarisameer512@gmail.com>
2024-02-17 22:40:44 +0530

    Notes

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: false,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+
+        1350: {
+          slidesPerView: 3,
+      },
+    },
+  });
